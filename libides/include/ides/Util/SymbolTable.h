8b0b9af3ece509f55f8e6dac964cd966e607cf62
seanedwards <sean.edwards@maine.edu>
2012-04-17 15:15:21 -0400

    Updated the project. Won't build, as semparse library is not public yet.

--- libides/include/ides/Util/SymbolTable.h
+++ libides/include/ides/Util/SymbolTable.h
@@ -1,5 +1,19 @@
 #ifndef _IDES_UTIL_SYMBOLTABLE_H_
 #define _IDES_UTIL_SYMBOLTABLE_H_
+#include <map>
+#include <string>
+#include <ides/common.h>
+
+namespace Ides {
+namespace Util {
+
+class IDES_EXPORTS SymbolTable {
+
+
+}; // class SymbolTable
+
+} // namespace Util
+} // namespace Ides
 
 #endif // _IDES_UTIL_SYMBOLTABLE_H_
 
---
+++ libides/include/ides/common.h
@@ -0,0 +1,11 @@
+#ifndef _IDES_COMMON_H_
+#define _IDES_COMMON_H_
+
+#ifdef WIN32
+
+#else
+#define IDES_EXPORTS
+#endif
+
+#endif // _IDES_COMMON_H_
+
