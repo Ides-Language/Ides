d143990c4d8dac98ca75261ad512c56d19ae27df
seanedwards <sean.edwards@maine.edu>
2012-04-18 21:00:00 -0400

    Updated CMake again and added windows declarations to common.h

--- libides/include/ides/common.h
+++ libides/include/ides/common.h
@@ -2,7 +2,11 @@
 #define _IDES_COMMON_H_
 
 #ifdef WIN32
-
+#ifdef LIBIDES_EXPORTS
+#define IDES_EXPORTS __declspec(dllexport)
+#else
+#define IDES_EXPORTS __declspec(dllimport)
+#endif
 #else
 #define IDES_EXPORTS
 #endif
