04e3ab8d356b318ee6f4e0e3e69500f27d9ee43f
Sean Edwards <stedwards87@gmail.com>
2012-12-22 00:15:25 -0500

    Implemented subtraction, multiplication and division on integer types.

--- libides/src/Types/IntegerOperators.cpp
+++ libides/src/Types/IntegerOperators.cpp
@@ -4,7 +4,8 @@
 namespace {
     typedef Ides::Types::ParseContext ParseContext;
     
-    const Ides::Types::Type* IntPlusType(ParseContext& ctx, Ides::AST::AST* lhs, Ides::AST::AST* rhs) {
+    template<char op>
+    const Ides::Types::Type* IntArithmeticType(ParseContext& ctx, Ides::AST::AST* lhs, Ides::AST::AST* rhs) {
         const Ides::Types::Type* rhstype = rhs->GetType(ctx);
         const Ides::Types::Type* lhstype = lhs->GetType(ctx);
         if (const Ides::Types::NumberType* rhsnumtype = dynamic_cast<const Ides::Types::NumberType*>(rhstype)) {
@@ -16,39 +17,74 @@ namespace {
             else if (lhsnumtype->HasImplicitConversionTo(rhsnumtype))
                 return rhsnumtype;
         }
-        throw Ides::Diagnostics::CompileError("could not resolve operator + for rhs argument of type " + rhstype->ToString(), rhs->exprloc);
+        std::stringstream err;
+        err << "could not resolve operator " << op << " for rhs argument of type " << rhstype->ToString();
+        throw Ides::Diagnostics::CompileError(err.str(), rhs->exprloc);
     }
     
-    llvm::Value* IntPlusValue(ParseContext& ctx, Ides::AST::AST* lhs, Ides::AST::AST* rhs) {
+#define MAKE_INT_ARITHMETIC_OPERATOR_VALUE(name, op, method) \
+    llvm::Value* Int##name##Value(ParseContext& ctx, Ides::AST::AST* lhs, Ides::AST::AST* rhs) { \
+        const Ides::Types::Type* rhstype = rhs->GetType(ctx); \
+        const Ides::Types::Type* lhstype = lhs->GetType(ctx); \
+        if (const Ides::Types::NumberType* rhsnumtype = dynamic_cast<const Ides::Types::NumberType*>(rhstype)) { \
+            const Ides::Types::NumberType* lhsnumtype = dynamic_cast<const Ides::Types::NumberType*>(lhstype); \
+            if (rhsnumtype->IsEquivalentType(lhsnumtype)) return ctx.GetIRBuilder()->method(lhs->GetValue(ctx), rhs->GetValue(ctx)); \
+            else if (rhsnumtype->HasImplicitConversionTo(lhsnumtype)) return ctx.GetIRBuilder()->method(lhs->GetValue(ctx), rhsnumtype->Convert(ctx, rhs->GetValue(ctx), lhsnumtype)); \
+            else if (lhsnumtype->HasImplicitConversionTo(rhsnumtype)) return ctx.GetIRBuilder()->method(lhsnumtype->Convert(ctx, lhs->GetValue(ctx), rhsnumtype), rhs->GetValue(ctx)); \
+        } \
+        throw Ides::Diagnostics::CompileError("could not resolve operator " op " for rhs argument of type ", rhs->exprloc); \
+    }
+    
+    MAKE_INT_ARITHMETIC_OPERATOR_VALUE(Plus, "+", CreateAdd);
+    MAKE_INT_ARITHMETIC_OPERATOR_VALUE(Sub, "-", CreateSub);
+    MAKE_INT_ARITHMETIC_OPERATOR_VALUE(Mul, "*", CreateMul);
+    
+    llvm::Value* IntDivideValue(ParseContext& ctx, Ides::AST::AST* lhs, Ides::AST::AST* rhs) {
         const Ides::Types::Type* rhstype = rhs->GetType(ctx);
         const Ides::Types::Type* lhstype = lhs->GetType(ctx);
         if (const Ides::Types::NumberType* rhsnumtype = dynamic_cast<const Ides::Types::NumberType*>(rhstype)) {
             const Ides::Types::NumberType* lhsnumtype = dynamic_cast<const Ides::Types::NumberType*>(lhstype);
             if (rhsnumtype->IsEquivalentType(lhsnumtype))
-                return ctx.GetIRBuilder()->CreateAdd(lhs->GetValue(ctx), rhs->GetValue(ctx));
+                return rhsnumtype->IsSigned() ?
+                    ctx.GetIRBuilder()->CreateSDiv(lhs->GetValue(ctx), rhs->GetValue(ctx)) :
+                    ctx.GetIRBuilder()->CreateUDiv(lhs->GetValue(ctx), rhs->GetValue(ctx));
             else if (rhsnumtype->HasImplicitConversionTo(lhsnumtype))
-                return ctx.GetIRBuilder()->CreateAdd(lhs->GetValue(ctx), rhsnumtype->Convert(ctx, rhs->GetValue(ctx), lhsnumtype));
+                return lhsnumtype->IsSigned() ?
+                ctx.GetIRBuilder()->CreateSDiv(lhs->GetValue(ctx), rhsnumtype->Convert(ctx, rhs->GetValue(ctx), lhsnumtype)) :
+                ctx.GetIRBuilder()->CreateUDiv(lhs->GetValue(ctx), rhsnumtype->Convert(ctx, rhs->GetValue(ctx), lhsnumtype));
             else if (lhsnumtype->HasImplicitConversionTo(rhsnumtype))
-                return ctx.GetIRBuilder()->CreateAdd(lhsnumtype->Convert(ctx, lhs->GetValue(ctx), rhsnumtype), rhs->GetValue(ctx));
+                return lhsnumtype->IsSigned() ?
+                    ctx.GetIRBuilder()->CreateSDiv(lhsnumtype->Convert(ctx, lhs->GetValue(ctx), rhsnumtype), rhs->GetValue(ctx)) :
+                    ctx.GetIRBuilder()->CreateUDiv(lhsnumtype->Convert(ctx, lhs->GetValue(ctx), rhsnumtype), rhs->GetValue(ctx));
         }
-        throw Ides::Diagnostics::CompileError("could not resolve operator + for rhs argument of type " + rhstype->ToString(), rhs->exprloc);
+        throw Ides::Diagnostics::CompileError("could not resolve operator / for rhs argument of type ", rhs->exprloc);
     }
 }
+
+#define MAKE_INT_ARITHMETIC_OPERATOR(op, pair) \
+    Integer8Type::GetSingletonPtr()->operators.insert(std::make_pair(op, pair)); \
+    UInteger8Type::GetSingletonPtr()->operators.insert(std::make_pair(op, pair)); \
+    Integer16Type::GetSingletonPtr()->operators.insert(std::make_pair(op, pair)); \
+    UInteger16Type::GetSingletonPtr()->operators.insert(std::make_pair(op, pair)); \
+    Integer32Type::GetSingletonPtr()->operators.insert(std::make_pair(op, pair)); \
+    UInteger32Type::GetSingletonPtr()->operators.insert(std::make_pair(op, pair)); \
+    Integer64Type::GetSingletonPtr()->operators.insert(std::make_pair(op, pair)); \
+    UInteger64Type::GetSingletonPtr()->operators.insert(std::make_pair(op, pair)); \
 
 
 namespace Ides {
 namespace Types {
     
     void VoidType::InitAllBaseTypeMembers() {
-        auto int_add = std::make_pair(IntPlusType, IntPlusValue);
-        Integer8Type::GetSingletonPtr()->operators.insert(std::make_pair("+", int_add));
-        UInteger8Type::GetSingletonPtr()->operators.insert(std::make_pair("+", int_add));
-        Integer16Type::GetSingletonPtr()->operators.insert(std::make_pair("+", int_add));
-        UInteger16Type::GetSingletonPtr()->operators.insert(std::make_pair("+", int_add));
-        Integer32Type::GetSingletonPtr()->operators.insert(std::make_pair("+", int_add));
-        UInteger32Type::GetSingletonPtr()->operators.insert(std::make_pair("+", int_add));
-        Integer64Type::GetSingletonPtr()->operators.insert(std::make_pair("+", int_add));
-        UInteger64Type::GetSingletonPtr()->operators.insert(std::make_pair("+", int_add));
+        auto int_add = std::make_pair(IntArithmeticType<'+'>, IntPlusValue);
+        auto int_subtract = std::make_pair(IntArithmeticType<'-'>, IntSubValue);
+        auto int_times = std::make_pair(IntArithmeticType<'*'>, IntMulValue);
+        auto int_divide = std::make_pair(IntArithmeticType<'/'>, IntDivideValue);
+        
+        MAKE_INT_ARITHMETIC_OPERATOR("+", int_add)
+        MAKE_INT_ARITHMETIC_OPERATOR("-", int_subtract)
+        MAKE_INT_ARITHMETIC_OPERATOR("*", int_times)
+        MAKE_INT_ARITHMETIC_OPERATOR("/", int_divide)
     }
     
 }
