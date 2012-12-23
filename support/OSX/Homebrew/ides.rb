5ec2976130742e9b8011f5e5f23d22244f28bca0
Sean Edwards <stedwards87@gmail.com>
2012-12-23 17:09:04 -0500

    Homebrew formula is now head-only, until we have actual releases.

--- support/OSX/Homebrew/ides.rb
+++ support/OSX/Homebrew/ides.rb
@@ -1,16 +1,13 @@
 require 'formula'
 
 class Ides < Formula
-	url 'https://github.com/Ides-Language/Ides/archive/master.zip'
+	head 'git://github.com/Ides-Language/Ides.git', :branch => 'master'
 	homepage 'http://ides-lang.com'
-	version '0.0'
-	#md5 '1234567890ABCDEF1234567890ABCDEF'
 
 	depends_on 'cmake'
 	depends_on 'boost'
 	depends_on 'llvm'
 	def install
-		#system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
 		system "cmake", ".", *std_cmake_args
 		system "make install"
 	end
