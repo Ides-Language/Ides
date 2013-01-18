724f9edd33a78737473c715ab70b334d9a13b977
Sean Edwards <stedwards87@gmail.com>
2013-01-18 18:33:11 -0500

    Trying another way of disabling git update

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -11,3 +11,6 @@ set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "dashboard.ides-lang.com")
 set(CTEST_DROP_LOCATION "/submit.php?project=Ides+Compiler")
 set(CTEST_DROP_SITE_CDASH TRUE)
+
+set(CTEST_UPDATE_TYPE "None")
+
