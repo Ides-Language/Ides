d1e75f409010ea5422350cd7085ae5adcf60a42b
Sean Edwards <stedwards87@gmail.com>
2014-01-08 01:02:51 -0500

    I love the boost preprocessor library.

--- ides/Parsing/AstVisitor.cpp
+++ ides/Parsing/AstVisitor.cpp
@@ -10,26 +10,11 @@
 
 namespace {
     DECL_AST_VISITOR(VisitAstVisitor, void, Ides::AstVisitor*);
-    template<> void VisitAstVisitor(const Ides::IdentifierExpr& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::ExprList& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::CallExpr& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::IndexExpr& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::ConstantExpr& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::BinaryExpr& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::UnaryExpr& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::Name& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::TraitDecl& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::ClassDecl& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::StructDecl& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::ValueDecl& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::ValDecl& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::VarDecl& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::FnDataDecl& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::FnDecl& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::ArgDecl& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::ModuleDecl& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::PartialFunction& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
-    template<> void VisitAstVisitor(const Ides::DataStructureDecl& ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
+
+
+#define IDES_AST_VISITOR_MEMBER_IMPL(r, data, elem) template<> \
+    void VisitAstVisitor(const Ides::elem & ast, Ides::AstVisitor* visitor) { return visitor->Visit(ast); }
+    BOOST_PP_SEQ_FOR_EACH(IDES_AST_VISITOR_MEMBER_IMPL, _, AST_TYPES)
 
 }
 
--- ides/Parsing/AstVisitor.h
+++ ides/Parsing/AstVisitor.h
@@ -25,26 +25,8 @@ namespace Ides {
 
         virtual void DoVisit(const Ides::AstBase& ast);
 
-        virtual void Visit(const IdentifierExpr& ast) { }
-        virtual void Visit(const ExprList& ast) { }
-        virtual void Visit(const CallExpr& ast) { }
-        virtual void Visit(const IndexExpr& ast) { }
-        virtual void Visit(const ConstantExpr& ast) { }
-        virtual void Visit(const BinaryExpr& ast) { }
-        virtual void Visit(const UnaryExpr& ast) { }
-        virtual void Visit(const Name& ast) { }
-        virtual void Visit(const TraitDecl& ast) { }
-        virtual void Visit(const ClassDecl& ast) { }
-        virtual void Visit(const StructDecl& ast) { }
-        virtual void Visit(const ValueDecl& ast) { }
-        virtual void Visit(const ValDecl& ast) { }
-        virtual void Visit(const VarDecl& ast) { }
-        virtual void Visit(const FnDataDecl& ast) { }
-        virtual void Visit(const FnDecl& ast) { }
-        virtual void Visit(const ArgDecl& ast) { }
-        virtual void Visit(const ModuleDecl& ast) { }
-        virtual void Visit(const PartialFunction& ast) { }
-        virtual void Visit(const DataStructureDecl& ast) { }
+#define IDES_AST_VISITOR_MEMBER(r, data, elem) virtual void Visit(const elem & ast) { }
+        BOOST_PP_SEQ_FOR_EACH(IDES_AST_VISITOR_MEMBER, _, AST_TYPES)
     };
 
     template<typename T>
@@ -52,51 +34,24 @@ namespace Ides {
     public:
         virtual ~AstVisitor() { }
 
-        virtual T DoAccept(const Ides::AstBase& ast) { DoVisit(ast, this); return last; }
+        virtual T DoAccept(const Ides::AstBase& ast) {
+            try {
+                DoVisit(ast, this);
+            } catch (...) {
+                last = NULL;
+                throw;
+            }
+            return last;
+        }
 
-        virtual T Accept(const IdentifierExpr& ast) { }
-        virtual T Accept(const ExprList& ast) { }
-        virtual T Accept(const CallExpr& ast) { }
-        virtual T Accept(const IndexExpr& ast) { }
-        virtual T Accept(const ConstantExpr& ast) { }
-        virtual T Accept(const BinaryExpr& ast) { }
-        virtual T Accept(const UnaryExpr& ast) { }
-        virtual T Accept(const Name& ast) { }
-        virtual T Accept(const TraitDecl& ast) { }
-        virtual T Accept(const ClassDecl& ast) { }
-        virtual T Accept(const StructDecl& ast) { }
-        virtual T Accept(const ValueDecl& ast) { }
-        virtual T Accept(const ValDecl& ast) { }
-        virtual T Accept(const VarDecl& ast) { }
-        virtual T Accept(const FnDataDecl& ast) { }
-        virtual T Accept(const FnDecl& ast) { }
-        virtual T Accept(const ArgDecl& ast) { }
-        virtual T Accept(const ModuleDecl& ast) { }
-        virtual T Accept(const PartialFunction& ast) { }
-        virtual T Accept(const DataStructureDecl& ast) { }
+
+#define IDES_AST_ACCEPTER_MEMBER(r, data, elem) virtual T Accept(const elem & ast) { }
+        BOOST_PP_SEQ_FOR_EACH(IDES_AST_ACCEPTER_MEMBER, _, AST_TYPES)
 
     private:
 
-        virtual void Visit(const IdentifierExpr& ast) { last = Accept(ast); }
-        virtual void Visit(const ExprList& ast) { last = Accept(ast); }
-        virtual void Visit(const CallExpr& ast) { last = Accept(ast); }
-        virtual void Visit(const IndexExpr& ast) { last = Accept(ast); }
-        virtual void Visit(const ConstantExpr& ast) { last = Accept(ast); }
-        virtual void Visit(const BinaryExpr& ast) { last = Accept(ast); }
-        virtual void Visit(const UnaryExpr& ast) { last = Accept(ast); }
-        virtual void Visit(const Name& ast) { last = Accept(ast); }
-        virtual void Visit(const TraitDecl& ast) { last = Accept(ast); }
-        virtual void Visit(const ClassDecl& ast) { last = Accept(ast); }
-        virtual void Visit(const StructDecl& ast) { last = Accept(ast); }
-        virtual void Visit(const ValueDecl& ast) { last = Accept(ast); }
-        virtual void Visit(const ValDecl& ast) { last = Accept(ast); }
-        virtual void Visit(const VarDecl& ast) { last = Accept(ast); }
-        virtual void Visit(const FnDataDecl& ast) { last = Accept(ast); }
-        virtual void Visit(const FnDecl& ast) { last = Accept(ast); }
-        virtual void Visit(const ArgDecl& ast) { last = Accept(ast); }
-        virtual void Visit(const ModuleDecl& ast) { last = Accept(ast); }
-        virtual void Visit(const PartialFunction& ast) { last = Accept(ast); }
-        virtual void Visit(const DataStructureDecl& ast) { last = Accept(ast); }
+#define IDES_AST_ACCEPTER_VISITOR_MEMBER(r, data, elem) virtual void Visit(const elem & ast) { last = Accept(ast); }
+        BOOST_PP_SEQ_FOR_EACH(IDES_AST_ACCEPTER_VISITOR_MEMBER, _, AST_TYPES)
 
         T last;
     };
